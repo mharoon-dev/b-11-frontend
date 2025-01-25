58dc9a80b5a79941b5ee4b3a1102c34ee6be5d5a
mharoon-dev <haroon.aghani@gmail.com>
2025-01-26 01:00:41 +0500

    add code

--- src/Pages/UserDashboard/UserDashboard.jsx
+++ src/Pages/UserDashboard/UserDashboard.jsx
@@ -1,13 +1,42 @@
 import AdminNavbar from "../../Components/AdminNavbar/AdminNavbar";
 import AdminSidebar from "../../Components/AdminSidebar/AdminSidebar";
+import "./UserDashboard.css";
 
 const UserDashboard = ( { isSidebarOpen,
     toggleSidebar,
     setIsSidebarOpen}) => {
   return (
     <>
+      <div className="admin-dashboard-container">
+      {/* Navbar */}
       <AdminNavbar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <AdminSidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+
+      {/* Sidebar and Main Content */}
+      <div className="admin-dashboard-content">
+        <AdminSidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+        <main className={`admin-main-content ${isSidebarOpen ? "" : "expanded"}`}>
+        <div className="admin-dashboard-stats">
+      <div className="admin-stat-card">
+        <h3>Total Students</h3>
+        <p>5000+</p>
+      </div>
+      <div className="admin-stat-card">
+        <h3>Cities</h3>
+        <p>05</p>
+      </div>
+      <div className="admin-stat-card">
+        <h3>Branches</h3>
+        <p>10</p>
+      </div>
+      <div className="admin-stat-card">
+        <h3>Courses</h3>
+        <p>24+</p>
+      </div>
+    </div>
+        </main>
+      </div>
+    </div>
     </>
   );
 };
